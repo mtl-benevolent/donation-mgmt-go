5f0b44dff6100c1a246a9ea13bec6cb0b750b7b7
Gabriel Lemire <gabriel.lemire@gmail.com>
2025-05-24 19:06:54 -0400

    Update the experiment to use a go template instead

--- src/cmd/experiments/generate_pdf.go
+++ src/cmd/experiments/generate_pdf.go
@@ -6,29 +6,80 @@ import (
 	"donation-mgmt/src/libs/logger"
 	"donation-mgmt/src/ptr"
 	"fmt"
+	"html/template"
 	"io"
 	"log/slog"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/oklog/ulid/v2"
 	"github.com/playwright-community/playwright-go"
+	"golang.org/x/text/currency"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
-const BAGE_PAGE_HTML = `
+const HTML_TEMPLATE = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>PDF Go generation experiment</title>
+  <title>PDF test</title>
 </head>
 <body>
-  <h1>Test PDF</h1>
-  <p>This PDF was generated using Go + Playwright</p>
+  <h1>{{ .Title }}</h1>
+  <p>{{ .Content }}</p>
+	<p>Money in fr-CA: {{ money .ReceiptAmountInCents "fr-CA" "CAD" }}
+	<p>Money in en-CA: {{ money .ReceiptAmountInCents "en-CA" "CAD" }}
+	<p>Generated on {{ .CreatedAt.Format "2006-01-02"  }}</p>
 </body>
 
 </html>
 `
+
+type TemplateValues struct {
+	Title                string
+	Content              string
+	ReceiptAmountInCents int64
+	CreatedAt            time.Time
+}
+
+func FormatMoney(cents int64, localeTag string, currencyCode string) (string, error) {
+	locale, err := language.Parse(localeTag)
+	if err != nil {
+		return "", fmt.Errorf("invalid locale: %w", err)
+	}
+
+	unit, err := currency.ParseISO(currencyCode)
+	if err != nil {
+		return "", fmt.Errorf("invalid currency code (%s): %w", currencyCode, err)
+	}
+
+	p := message.NewPrinter(locale)
+
+	amount := float64(cents) / 100
+	result := p.Sprintf("%v", currency.NarrowSymbol(unit.Amount(amount)))
+
+	return result, nil
+}
+
+func getHtml(value TemplateValues) (string, error) {
+	tmpl, err := template.New("pdf-receipt").Funcs(template.FuncMap{
+		"money": FormatMoney,
+	}).Parse(HTML_TEMPLATE)
+	if err != nil {
+		return "", fmt.Errorf("error parsing template: %w", err)
+	}
+
+	sb := &strings.Builder{}
+	if err := tmpl.Execute(sb, value); err != nil {
+		return "", fmt.Errorf("error rendering template: %w", err)
+	}
+
+	return sb.String(), nil
+}
 
 func exec(l *slog.Logger) error {
 	pw, err := playwright.Run(&playwright.RunOptions{
@@ -74,8 +125,19 @@ func exec(l *slog.Logger) error {
 		return fmt.Errorf("error creating new page: %w", err)
 	}
 
+	l.Info("Rendering the template")
+	htmlContent, err := getHtml(TemplateValues{
+		Title:                "My awesome PDF",
+		Content:              "<script>alert('hack attempt');</script>",
+		ReceiptAmountInCents: 5996,
+		CreatedAt:            time.Now(),
+	})
+	if err != nil {
+		return err
+	}
+
 	l.Info("Setting the HTML content of the page")
-	if err = page.SetContent(BAGE_PAGE_HTML, playwright.PageSetContentOptions{
+	if err = page.SetContent(htmlContent, playwright.PageSetContentOptions{
 		WaitUntil: playwright.WaitUntilStateDomcontentloaded,
 	}); err != nil {
 		return fmt.Errorf("error setting the HTML content for the page: %w", err)
